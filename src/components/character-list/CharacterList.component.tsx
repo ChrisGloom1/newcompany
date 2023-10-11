ce919590bdf41f315670410fa5fb230f1ef8b151
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2023-10-11 11:45:36 +0200

    completetd the sort and search function

--- src/components/character-list/CharacterList.component.tsx
+++ src/components/character-list/CharacterList.component.tsx
@@ -5,62 +5,95 @@ import CharacterItem from "../character-item/CharacterItem.component";
 import ICharacter from "../../interfaces/character/ICharacter";
 
 const CharacterList = () => {
+  const [search, setSearch] = useState<string>("");
+  const [sortOption, setSortOption] = useState("idAscending");
+  const { characters, getCharacters } = useContext(CharacterContext) as ICharacterContext;
 
-  const [search, setSearch] = useState<ICharacter[]>([]);
-  const {characters, getCharacters} = useContext(CharacterContext) as ICharacterContext
-
-  useEffect (() => {
+  useEffect(() => {
     getCharacters();
-  }, [getCharacters])
-
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => e.preventDefault()
+  }, [getCharacters]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.value || e.target.value === '') {
-      setSearch(characters)
-      } else {
-      const resultsArray = 
-        characters.filter(char => char.name.toLowerCase().includes(e.target.value.toLowerCase()))
-      setSearch(resultsArray)
+    const query = e.target.value;
+    setSearch(query);
+  };
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const option = e.target.value;
+    setSortOption(option);
+  };
+
+  const sortCharacters = (charactersToSort: ICharacter[], sortingOption: string) => {
+    switch (sortingOption) {
+      case "idAscending":
+        return [...charactersToSort].sort((a, b) => a.id - b.id);
+      case "idDescending":
+        return [...charactersToSort].sort((a, b) => b.id - a.id);
+      case "nameAscending":
+        return [...charactersToSort].sort((a, b) => a.name.localeCompare(b.name));
+      case "nameDescending":
+        return [...charactersToSort].sort((a, b) => b.name.localeCompare(a.name));
     }
-  }
+    return charactersToSort;
+  };
 
-  const searchResults = search.map((char: ICharacter, i: number) => (
-    <CharacterItem
-      key={`character-${i}`}
-      id={char.id}
-      name={char.name}
-      gender={char.gender}
-      status={char.status}
-      image={char.image}
-    />
-  ))
+  const searchResults = characters.filter((char) =>
+    char.name.toLowerCase().includes(search.toLowerCase())
+  );
 
-  const charactersLoaded = characters.map((char: ICharacter, i: number) => (
-    <CharacterItem
-      key={`character-${i}`}
-      id={char.id}
-      name={char.name}
-      gender={char.gender}
-      status={char.status}
-      image={char.image}
-    />
-  ))
+  const sortedCharacters = sortCharacters(searchResults, sortOption);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleSearchChange}/>
-        <button>Search</button>
-      </form>
+    <>
+      <div className="row">
+        <div className="col-md-6">
+          <form className="form-group">
+            <label htmlFor="search">Search:</label>
+            <input
+              id="search"
+              type="text"
+              value={search}
+              onChange={handleSearchChange}
+              className="form-control mb-4"
+              placeholder="Search for character..."
+            />
+          </form>
+        </div>
+        <div className="col-md-6">
+          <form className="form-group">
+            <label htmlFor="sort">Sort by:</label>
+            <select
+              id="sort"
+              className="form-control mb-4"
+              value={sortOption}
+              onChange={handleSortChange}
+            >
+              <option value="idAscending">Id (Ascending)</option>
+              <option value="idDescending">Id (Descending)</option>
+              <option value="nameAscending">Name (Ascending)</option>
+              <option value="nameDescending">Name (Descending)</option>
+            </select>
+          </form>
+        </div>
+      </div>
       <section className="row gy-4">
-        {searchResults.length > 0
-          ? searchResults
-          : charactersLoaded}
+        {sortedCharacters.length > 0 ? (
+          sortedCharacters.map((char: ICharacter, i: number) => (
+            <CharacterItem
+              key={`character-${char.id}`}
+              id={char.id}
+              name={char.name}
+              gender={char.gender}
+              status={char.status}
+              image={char.image}
+            />
+          ))
+        ) : (
+          <p>No characters available</p>
+        )}
       </section>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-  export default CharacterList;
+export default CharacterList;
